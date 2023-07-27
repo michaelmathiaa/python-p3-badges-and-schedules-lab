d3a52fe07d89fbc3e6c38afa229e7876c253492d
Michael Mathias <michaelscodingjourney@gmail.com>
2023-07-27 16:01:15 -0700

    Passed tests

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,19 @@
 def badge_maker(name):
-    return None
+    return f'Hello, my name is {name}.'
 
 def batch_badge_creator(names):
-    return None
+    return [f'Hello, my name is {name}.' for name in names]
 
 def assign_rooms(names):
-    return None
+    index = 1
+    greeting_list = []
+    for name in names:
+        greeting_list.append(f"Hello, {name}! You'll be assigned to room {index}!")
+        index += 1
+    return greeting_list
 
 def printer(names):
-    return None
+    for badge_creator in batch_badge_creator(names):
+        print(badge_creator)
+    for greeting in assign_rooms(names):
+        print(greeting)    
